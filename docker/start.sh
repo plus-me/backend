cc1b892e043dd7c155cc12641cee1629be41233e
Tim Schumacher <tim@datenknoten.me>
2022-03-27 21:52:16 +0200

    fix: use sync worker class

--- docker/start.sh
+++ docker/start.sh
@@ -15,7 +15,7 @@ if [ "$PROCESS_TYPE" = "server" ]; then
             --reload \
             --bind 0.0.0.0:8000 \
             --workers 2 \
-            --worker-class eventlet \
+            --worker-class sync \
             --log-level DEBUG \
             --access-logfile "-" \
             --error-logfile "-" \
@@ -24,7 +24,7 @@ if [ "$PROCESS_TYPE" = "server" ]; then
         gunicorn \
             --bind 0.0.0.0:8000 \
             --workers 2 \
-            --worker-class eventlet \
+            --worker-class sync \
             --log-level DEBUG \
             --access-logfile "-" \
             --error-logfile "-" \
